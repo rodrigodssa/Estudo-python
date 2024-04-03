a7fca77fd9d08a131b5f6e891028e8e75667408e
rodrigodssa <rodrigodssalmeida@gmail.com>
2024-04-02 21:25:40 -0300

    primeiro commit

---
+++ operacoes.py
@@ -0,0 +1,29 @@
+# operacoes numericas
+faturamento = 1000
+custo = 700
+
+novas_vendas = 300
+
+faturamento = faturamento + novas_vendas # somar
+imposto = faturamento * 0.1 # multiplicar
+lucro = faturamento - custo
+
+print(faturamento)
+print(lucro)
+margem_lucro = lucro / faturamento # dividir
+print(margem_lucro)
+
+restituicao = imposto * 0.1
+print(restituicao)
+
+# Mod resto da divisao
+# 10 % 3
+tempo_em_meses = 160
+tempo_em_anos = int(tempo_em_meses / 12)
+print(tempo_em_anos , "anos")
+print(tempo_em_meses % 12)
+
+numero = 123.97
+print(round(numero))
+
+faturamento = 139_018_182 # edicao visual para facilitar 
---
+++ texto.py
@@ -0,0 +1,14 @@
+faturamento = 1000
+custo = 700
+
+lucro = faturamento - custo
+
+print(f"faturamento: {faturamento}, custo: {custo}, lucro: {lucro}")
+print("faturamento:" + str(faturamento) + ", + custo : " + (custo) + ", lucro: " + str(lucro))
+
+email = "Email_falso@GMAIL.COM"
+
+print(email.lower())
+
+
+
