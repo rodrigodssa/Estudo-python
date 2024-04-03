ab7e03d0990803a4e85d57b88194647de3f699c7
rodrigodssa <rodrigodssalmeida@gmail.com>
2024-04-02 21:10:16 -0300

    primeiro commit

---
+++ codigo.py
@@ -0,0 +1,27 @@
+# faturamento numero  -> inteiro
+
+faturamento = 1200
+custo = 770
+
+novas_vendas = 300
+faturamento = faturamento + novas_vendas
+
+# numero decimal -> float
+taxa_imposto = 0.1
+
+# string = texto = str
+mensagem = "o faturamento foi de "
+
+# boolean
+teve_lucro = False
+
+imposto = taxa_imposto * faturamento
+print("Faturamento" , faturamento)
+print("custo" , custo)
+print("lucro" , faturamento - custo - imposto)
+print(mensagem, faturamento)
+
+
+
+
+
