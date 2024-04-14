11ce4f88755898e1402445a629bbc2b90117bb13
rodrigodssa <rodrigodssalmeida@gmail.com>
2024-04-14 17:57:01 -0300

    novo commit

--- texto.py
+++ texto.py
@@ -3,8 +3,8 @@ custo = 700
 
 lucro = faturamento - custo
 
-# print(f"Faturamento: {faturamento}, Custo: {custo}, Lucro: {lucro}")
-# print("Faturamento:" + str(faturamento) + ", Custo:" + str(custo) + ", Lucro: " + str(lucro))
+print(f"Faturamento: {faturamento}, Custo: {custo}, Lucro: {lucro}")
+print("Faturamento:" + str(faturamento) + ", Custo:" + str(custo) + ", Lucro: " + str(lucro))
 
 email = "EMAIL_falso@gmail.com"
 
