5f98af51c3cf9e1298709f2df27b648f2c433be3
rodrigodssa <rodrigodssalmeida@gmail.com>
2024-04-15 19:27:42 -0300

    novo commit

--- inputs2.py
+++ inputs2.py
@@ -1,5 +1,13 @@
 # 1% das vendas
-vendas = input("Digite suas vendas do dia: ")
+vendas = input("Digite suas vendas do dia: ") # segunda forma seria vendas = float(input("Digite suas vendas do dia: ")
+vendas = float(vendas)
 
 Bonus = vendas * 0.01
 print(Bonus)
+
+# cuidado
+vendas_dia1 = input("vendas dia 1:")
+vendas_dia2 = input("vendas dia 2:")
+
+
+print(f"total de vendas {float(vendas_dia1) + float(vendas_dia2)} ")
